b2ff8c45eff13ec9725b6de249e663f74fd0ba0f
Mikel Reyes <mikelsreyes@gmail.com>
2023-11-01 08:21:28 -0400

    init method

--- sad_game.py
+++ sad_game.py
@@ -7,4 +7,8 @@ class Car:
         model (str): the model of the car
         make (str): the make of the car
         year (str): the year the car was made"""
-        
+    
+    def __init__(self, model, make, year):
+        self.model = model
+        self.make = make
+        self.year = year
