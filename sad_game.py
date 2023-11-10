d0a453005e935ecfa1b3f6a6dfd17c79f583c173
Donovan <turbine4266@gmail.com>
2023-11-10 11:28:55 -0500

    11

--- sad_game.py
+++ sad_game.py
@@ -3,7 +3,7 @@
 
 """ A turn based adventure game where the character will make choices that affect their final battle with the big boss
 """
-class Car:
+class Car: #### WHY DO WE HAVE A CAR IN HERE???
     """A car in a dealership
     
     Attributes:
@@ -11,17 +11,60 @@ class Car:
         make (str): the make of the car
         year (str): the year the car was made"""
     
-    def __init__(self, model, make, year):
-        self.model = model
-        self.make = make
-        self.year = year
-        
-        
-class Character():
-    def __init__(self, name, skill, weapon, health):
+class Character:
+    """Its a character, all right. 
+    Attributes:
+    alive(int): evaluates if hp is above 0
+    take_damage(int): reduces hp based on damage taken
+    
+    Side effects:
+    """
+    def __init__(self, name, hp, damage, weapon, skill, speed, moves):
         self.name = name
-        self.skill = skill
+        self.hp = hp
+        self.damage = damage
         self.weapon = weapon
-        self.health = health 
-        #stage
+        self.skill = skill
+        self.speed = speed
+        self.moves = moves
+        
+    def alive(self):
+        return self.hp > 0
+    
+    def take_damage(self, damage):
+        self.hp -= damage
+        
+class Turn:
+    """Making the turn-based combat system
+    Attributes:
+    __init__():
+    attack(int): to damage the other character
+    
+    Side effects: reduce hp of of the target
+    """
+    def __init__(Character):
+        pass
+        
+    def attack(self, attacker, target, weapon):
+        damage_delt = attacker.damage + weapon.damage
+        target.take_damage(damage_delt)
+        
+          
+def battle():
+    """FIGHT!
+    Args(): IDK YET 
+    
+    Returns:
+    Who will win the big battle? Will you beat this game? Probably.
+    """
+    hero = Character(self.name, self.hp, self.damage, self.weapon, self.skill, self.speed, self.moves)
+    Tzeech = Character("Tzeech", 100, 12, "Great Sythe", 
+                       "Intimidate", 3, 2)
+    while hero.alive and Tzeech.alive:
+        pass
+        
+    if hero.alive:
+        pass
+    else:
+        print("You have died.")
         
